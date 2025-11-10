91a52653eb00c9aeb70a3741c7c08f5f1916c342
nguyendinhbach2015 <nguyendinhbach2015@gmail.com>
2025-11-10 17:44:16 +0100

    update

--- ECommerceAPI/Program.cs
+++ ECommerceAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
   options.AddPolicy("AllowAngularApp", policy =>
   {
-    policy.WithOrigins("http://localhost:4200") 
+    policy.WithOrigins("http://localhost:4200", "http://15.237.174.42:4200") 
           .AllowAnyHeader()
           .AllowAnyMethod();
   });
